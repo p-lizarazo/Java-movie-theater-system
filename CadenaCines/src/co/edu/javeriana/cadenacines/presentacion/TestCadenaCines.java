557b1171cf96c57f3e99ea656be4583b6807bf99
orozcojuan1998 <ingjuanorozco@outlook.com>
2016-09-05 20:52:35 -0500

    Pequeños cambios en el main

--- CadenaCines/src/co/edu/javeriana/cadenacines/presentacion/TestCadenaCines.java
+++ CadenaCines/src/co/edu/javeriana/cadenacines/presentacion/TestCadenaCines.java
@@ -1,9 +1,73 @@
 package co.edu.javeriana.cadenacines.presentacion;
 
+import java.util.Scanner;
+
 public class TestCadenaCines {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		
+		System.out.println("SISTEMA CADENACINES");
+		System.out.println("    "+"opcion 1:   ingresar centros comerciales, cines y sillas asociadas");
+		System.out.println("    "+"opcion 2:   reporte de centros comerciales, cines y sillas asociadas");
+		System.out.println("    "+"opcion 3:   ingresar clientes");
+		System.out.println("    "+"opcion 4:   ingresar peliculas");
+		System.out.println("    "+"opcion 5:   agregar una funcion asociada a una pelicula y a un cine");
+		System.out.println("    "+"opcion 6:   consultar funciones para una pelicula");
+		System.out.println("    "+"opcion 7:   comprar boletas para una funcion");
+		System.out.println("    "+"opcion 8:   reporte de clientes con boletas compradas");
+		System.out.println("    "+"opcion 9:   salir");
+		
+		System.out.println("Digite una opcion : ");
+		String x = scanner.nextLine();
+		int opcion = Integer.parseInt(x);
+		
+		if(opcion == 1){
+			
+		}
+		
+		else if(opcion == 2){
+					
+		}
+
+		else if(opcion == 3){
+			
+		}
+
+
+		else if(opcion == 4){
+			
+		}
+
+
+
+		else if(opcion == 5){
+			
+		}
+
+		else if(opcion == 6){
+			
+		}
+
+		if(opcion == 7){
+			
+		}
+
+		if(opcion == 8){
+			
+		}
+
+
+		if(opcion == 9){
+			
+		}
+		
+		
 
 	}
 
