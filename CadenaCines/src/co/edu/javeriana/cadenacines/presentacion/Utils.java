53f9786d0504143cb65a69ce470fda3c2ffa0d7d
orozcojuan1998 <ingjuanorozco@outlook.com>
2016-09-08 19:48:39 -0500

    Se agrego el metodo para el uso de fechas por medio de localdate time en la clase Utils

--- CadenaCines/src/co/edu/javeriana/cadenacines/presentacion/Utils.java
+++ CadenaCines/src/co/edu/javeriana/cadenacines/presentacion/Utils.java
@@ -1,5 +1,18 @@
 package co.edu.javeriana.cadenacines.presentacion;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Utils {
+	
+	public LocalDateTime convertirStringFecha(String anio,String mes, String dia, String hora,String minutos){
+		String fecha = anio+"-"+mes+"-"+dia+" "+hora+":"+minutos;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		LocalDateTime fechaL = LocalDateTime.parse(fecha,formatter);
+		
+		return fechaL;
+		
+	}
 
 }
